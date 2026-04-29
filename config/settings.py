2f1c0a853ae3d5671954ae95a743b0c7992bd683
Juls00 <juliagimena00@alumnos.cei.es>
2026-04-29 20:23:29 +0200

    Añadida configuración Docker

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = 'django-insecure-s0vt&dxc*-o(0)c$s80glld&phsc$f24#_)v3vezo(p#hsc@4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
